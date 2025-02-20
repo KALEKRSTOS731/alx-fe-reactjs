ba0af2bec9899a2b8011fe5741c690b44416ff12
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-02-20 22:13:46 +0300

    alx-react-app-new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,4 @@
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
+
 export default MainContent;
 <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
 <p>I love to visit New York, Paris, and Tokyo.</p>
