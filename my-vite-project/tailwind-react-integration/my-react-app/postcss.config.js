1b2012aa70a95f9016a513be2eb4ca01c8aa697a
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-03-15 17:36:15 +0000

    new

--- my-vite-project/tailwind-react-integration/my-react-app/postcss.config.js
+++ my-vite-project/tailwind-react-integration/my-react-app/postcss.config.js
@@ -4,3 +4,10 @@ module.exports = {
     autoprefixer: {},
   },
 }
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
