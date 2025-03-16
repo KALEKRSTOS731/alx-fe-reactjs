5b6bc4df06375494934c8799f2c1f42d73d1e6f9
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-03-16 10:22:48 -0700

    Update postcss.config.js

--- tailwind-react-integration/postcss.config.js
+++ tailwind-react-integration/postcss.config.js
@@ -1,1 +1,7 @@
-
+   module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
