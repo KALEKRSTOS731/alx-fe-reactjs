b17acdb7f668211f45492d0e42169a8337f9f435
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-03-29 22:18:23 +0000

    git

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,33 @@
 import axios from "axios";
 
-const API_URL = "https://api.github.com/search/users?q";
+const BASE_URL = "https://api.github.com/search/users?q=";
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async (username, location, minRepos) => {
   try {
-    const response = await axios.get(`${API_URL}/${username}`);
-    return response.data;
+    let query = `${username}`;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>${minRepos}`;
+
+    const response = await axios.get(`${BASE_URL}${query}`, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+      },
+    });
+
+    if (response.data.items.length > 0) {
+      const user = response.data.items[0];
+      const userDetails = await axios.get(user.url, {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+        },
+      });
+      return userDetails.data;
+    }
+    return null;
   } catch (error) {
-    console.error("Error fetching GitHub user data:", error);
+    console.error("Error fetching user data:", error);
     return null;
   }
 };
+
 
