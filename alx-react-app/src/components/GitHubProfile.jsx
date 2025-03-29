70e0f4abca8ce5b7c4db839d94ac19a41bd10b66
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-03-29 09:44:38 -0700

    Update GitHubProfile.jsx

--- alx-react-app/src/components/GitHubProfile.jsx
+++ alx-react-app/src/components/GitHubProfile.jsx
@@ -1,1 +1,23 @@
+import { useState, useEffect } from "react";
+import { fetchGitHubUser } from "../services/api";
+
+const GitHubProfile = ({ username }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetchGitHubUser(username).then((data) => setUser(data));
+  }, [username]);
+
+  return user ? (
+    <div>
+      <h2>{user.name}</h2>
+      <p>{user.bio}</p>
+      <img src={user.avatar_url} alt={user.name} width={100} />
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+export default GitHubProfile;
 
